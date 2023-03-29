61d18ed99acb67e721b07b4d2e36d6c2ccc92a9c
unrealdinnerbone@gmail.com <unrealdinnerbone@gmail.com>
2023-03-29 12:35:58 -0500

    Add Hashed Ip and User-Anget

--- src/main/java/com/unrealdinnerbone/javalinutils/InfluxPlugin.java
+++ src/main/java/com/unrealdinnerbone/javalinutils/InfluxPlugin.java
@@ -11,6 +11,7 @@ import io.javalin.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.Instant;
+import java.util.Objects;
 
 public class InfluxPlugin implements Plugin {
 
@@ -30,8 +31,15 @@ public class InfluxPlugin implements Plugin {
     @Override
     public void apply(@NotNull Javalin javalin) {
         javalin.cfg.requestLogger.http((ctx, executionTimeMs) -> {
+            String ip = ctx.header("X-Forwarded-For");
+            if(ip == null || ip.isEmpty()) {
+                ip = ctx.ip();
+            }
+            String userAgent = ctx.header("User-Agent");
             Point point = Point.measurement("requests")
                     .addField("execution_time", executionTimeMs)
+                    .addTag("ip", String.valueOf(Objects.hashCode(ip)))
+                    .addTag("user_agent", userAgent)
                     .addTag("path", ctx.path())
                     .addTag("status", String.valueOf(ctx.status().getCode()))
                     .addTag("method", ctx.method().name().toLowerCase())
